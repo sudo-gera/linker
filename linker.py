db29e01b95faae0bc4839d9f23f6935b0b883469
sudo-gera <gera.tatarinov.999@gmail.com>
2020-08-19 20:36:28 +0300

    hash&quiet

--- linker.py
+++ linker.py
@@ -8,6 +8,11 @@ from json import dumps
 from os import getenv
 from os import popen
 from subprocess import check_output as output
+from sys import argv
+
+quiet=0
+if '-q' in argv:
+    quiet=1
 
 gi.require_version('Gtk', '3.0')
 exec('from gi.repository import Gtk')
@@ -39,15 +44,23 @@ ps = open('linker.ps1', 'w')
 count = 0
 lc = 0
 wid = get_terminal_size()[0]
+from time import time
+t=dict()
 for w in d:
+
+
+    if quiet==0:
+        pc = int(count / len(d) * wid)
+        print(pc * '#', end='\r')
+        count += 1
+
+    new=0
+
     try:
         a = open(w + '.desktop').read()
     except BaseException:
         print(error())
 
-    pc = int(count / len(d) * wid)
-    print(pc * '#', end='\r')
-    count += 1
 
     comm = get('exec', a, None)
     if comm is None or get('nodisplay', a, 'false').lower().strip() == 'true':
@@ -60,40 +73,61 @@ for w in d:
     for e in '\\/:*?"<>|+%!@':
         name = name.replace(e, '_')
 
-    icon = [Gtk.lookup_icon(get('icon', a, 'application-x-executable'), w, 0)
-            for w in [48] + list(range(512))]
-    icon += [Gtk.lookup_icon('application-x-executable', w, 0)
-             for w in [48] + list(range(512))]
-    icon = [w.get_filename() for w in icon if w][0]
+    if name not in db:
+        db[name]=dict()
+        new=1
 
-    hash = output(['sum', icon]).decode().strip()
-    if icon not in db or db[icon] != hash:
-        db[icon] = hash
+    icon = None
+    for w in [48]+list(range(512)):
+        if icon:
+            break
+        icon=Gtk.lookup_icon(get('icon', a, 'application-x-executable'), w, 0)
+    for w in [48]+list(range(512)):
+        if icon:
+            break
+        icon=Gtk.lookup_icon('application-x-executable', w, 0)
+
+    icon = icon.get_filename()
+
+    hash = str(sum([w*2**(e%60) if e%2 else -w*2**(e%60) for e,w in enumerate(open(icon,'rb').read())]))
+    if icon not in db[name] or db[name][icon] != hash:
+        db[name][icon] = hash
         sh(['convert', icon, name + '.ico'])
+        new=1
 
-    sh(['cp', w + '.desktop', name + '.desktop'])
+#    sh(['cp', w + '.desktop', name + '.desktop'])
 
-    b = open(name + '.sh', 'w')
-    b.write('export DISPLAY=localhost:0.0; ' + comm)
-    b.close()
+    script='export DISPLAY=localhost:0.0; ' + comm
+    if 'sh' not in db[name] or db[name]['sh']!=script:
+        b = open(name + '.sh', 'w')
+        b.write(script)
+        b.close()
+        db[name]['sh']=script
 
-    b = open(name + '.bat', 'w')
-    b.write(
-        'wsl "/mnt/c/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/' +
-        name +
-        '.sh"')
-    b.close()
+    script='wsl "/mnt/c/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/'+name +'.sh"'
+    if 'bat' not in db[name] or db[name]['bat']!=script:
+        b = open(name + '.bat', 'w')
+        b.write(script)
+        b.close()
+        db[name]['bat']=script
 
-    ps.write(f'''
+    if new:
+        ps.write(f'''
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("c:/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/{name}.lnk")
 $Shortcut.TargetPath = "c:/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/{name}.bat"
 $Shortcut.IconLocation = "c:/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/{name}.ico"
 $Shortcut.Save()
+''')
+    if quiet==0:
+        ps.write(f'''
 Write-Host -NoNewline "{'#'*(pc-lc)}"
 ''')
-    lc = pc
+        lc = pc
 ps.close()
+if quiet==0:
+    print(' '*wid)
 sh(['powershell.exe', './linker.ps1'])
-print()
+if quiet==0:
+    print(' '*wid)
 open(getenv('HOME') + '/.linker', 'w').write(dumps(db))
