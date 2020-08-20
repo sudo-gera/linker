4426179e4062de2634ce5e9d0b8c6e7aa9017c75
sudo-gera <gera.tatarinov.999@gmail.com>
2020-08-20 11:09:16 +0300

    loading

--- linker.py
+++ linker.py
@@ -1,3 +1,4 @@
+from time import time
 import gi
 from subprocess import run as sh
 import os
@@ -10,13 +11,13 @@ from os import popen
 from subprocess import check_output as output
 from sys import argv
 
-quiet=0
+quiet = 0
 if '-q' in argv:
-    quiet=1
+    quiet = 1
 
-slow=0
+slow = 0
 if '-s' in argv:
-    slow=1
+    slow = 1
 
 gi.require_version('Gtk', '3.0')
 exec('from gi.repository import Gtk')
@@ -32,6 +33,9 @@ def get(q, a, z):
 
 Gtk = Gtk.IconTheme.get_default()
 
+os.chdir('/mnt/c/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/')
+if not os.path.exists('./wsl/'):
+    sh(['mkdir', './wsl/'])
 os.chdir('/mnt/c/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/')
 
 try:
@@ -48,25 +52,25 @@ ps = open('linker.ps1', 'w')
 count = 0
 lc = 0
 wid = get_terminal_size()[0]
-from time import time
-t=dict()
+t = dict()
 for w in d:
 
     if slow:
         sleep(0.01)
 
-    if quiet==0:
+    if quiet == 0:
         pc = int(count / len(d) * wid)
         print(pc * '#', end='\r')
         count += 1
 
-    new=0
+    new = 0
 
     try:
         a = open(w + '.desktop').read()
     except BaseException:
-        print(error())
-
+        err = [w for w in error().split('\n') if w][-1]
+        print(' ' * wid, end='\r')
+        print(err)
 
     comm = get('exec', a, None)
     if comm is None or get('nodisplay', a, 'false').lower().strip() == 'true':
@@ -80,42 +84,44 @@ for w in d:
         name = name.replace(e, '_')
 
     if name not in db:
-        db[name]=dict()
-        new=1
+        db[name] = dict()
+        new = 1
 
     icon = None
-    for w in [48]+list(range(512)):
+    for w in [48] + list(range(512)):
         if icon:
             break
-        icon=Gtk.lookup_icon(get('icon', a, 'application-x-executable'), w, 0)
-    for w in [48]+list(range(512)):
+        icon = Gtk.lookup_icon(
+            get('icon', a, 'application-x-executable'), w, 0)
+    for w in [48] + list(range(512)):
         if icon:
             break
-        icon=Gtk.lookup_icon('application-x-executable', w, 0)
+        icon = Gtk.lookup_icon('application-x-executable', w, 0)
 
     icon = icon.get_filename()
 
-    hash = str(sum([w*2**(e%60) if e%2 else -w*2**(e%60) for e,w in enumerate(open(icon,'rb').read())]))
+    hash = str(sum([w * 2**(e % 60) if e % 2 else -w * 2**(e % 60)
+                    for e, w in enumerate(open(icon, 'rb').read())]))
     if icon not in db[name] or db[name][icon] != hash:
         db[name][icon] = hash
         sh(['convert', icon, name + '.ico'])
-        new=1
+        new = 1
 
 #    sh(['cp', w + '.desktop', name + '.desktop'])
 
-    script='export DISPLAY=localhost:0.0; ' + comm
-    if 'sh' not in db[name] or db[name]['sh']!=script:
+    script = 'export DISPLAY=localhost:0.0; ' + comm
+    if 'sh' not in db[name] or db[name]['sh'] != script:
         b = open(name + '.sh', 'w')
         b.write(script)
         b.close()
-        db[name]['sh']=script
+        db[name]['sh'] = script
 
-    script='wsl "/mnt/c/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/'+name +'.sh"'
-    if 'bat' not in db[name] or db[name]['bat']!=script:
+    script = 'wsl "/mnt/c/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/' + name + '.sh"'
+    if 'bat' not in db[name] or db[name]['bat'] != script:
         b = open(name + '.bat', 'w')
         b.write(script)
         b.close()
-        db[name]['bat']=script
+        db[name]['bat'] = script
 
     if new:
         ps.write(f'''
@@ -125,15 +131,15 @@ $Shortcut.TargetPath = "c:/Users/user/AppData/Roaming/Microsoft/Windows/Start Me
 $Shortcut.IconLocation = "c:/Users/user/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/wsl/{name}.ico"
 $Shortcut.Save()
 ''')
-    if quiet==0:
+    if quiet == 0:
         ps.write(f'''
 Write-Host -NoNewline "{'#'*(pc-lc)}"
 ''')
         lc = pc
 ps.close()
-if quiet==0:
-    print(' '*wid)
+if quiet == 0:
+    print(' ' * wid, end='\r')
 sh(['powershell.exe', './linker.ps1'])
-if quiet==0:
-    print(' '*wid)
+if quiet == 0:
+    print('\r' + ' ' * wid, end='\r')
 open(getenv('HOME') + '/.linker', 'w').write(dumps(db))
